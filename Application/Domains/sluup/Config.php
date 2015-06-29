d6f2a8dbb4de64711cf7300fbb9de168549119ad
Kelvin Souza <kelvin.souza@tblmanager.com.br>
2015-06-29 19:23:04 -0300

    bug icons no crhome e setando bd no prod

--- Application/Domains/sluup/Config.php
+++ Application/Domains/sluup/Config.php
@@ -1,13 +1,24 @@
 <?php
 
-$Conf['Database'] = array(
-	'Server' 	=> 'localhost',
-	'Global' 	=> 'labs',
-	'User'   	=> 'root',
-	'Password' 	=> '',
-	'Type' 		=> 'MySQL',
-	'Decode'    => 'utf8'
-);
+if( getenv("PARAM1") == 'DEV' ){
+	$Conf['Database'] = array(
+		'Server' 	=> 'localhost',
+		'Global' 	=> 'labs',
+		'User'   	=> 'root',
+		'Password' 	=> '',
+		'Type' 		=> 'MySQL',
+		'Decode'    => 'utf8'
+	);
+} else if( getenv("PARAM1") == 'PROD' ){
+	$Conf['Database'] = array(
+		'Server' 	=> '127.0.0.1',
+		'Global' 	=> 'labs',
+		'User'   	=> 'root',
+		'Password' 	=> 'Q4w3e2r1',
+		'Type' 		=> 'MySQL',
+		'Decode'    => 'utf8'
+	);
+}
 
 define( 'BD', 'labs' );
 define( 'Title', 'Sluup Labs' );
