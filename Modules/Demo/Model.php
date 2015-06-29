6ff29f9c318a5903b33780f1ad6f266dbb7eca46
Kelvin Souza <kelvin.souza@tblmanager.com.br>
2015-06-29 12:59:38 -0300

    nada

--- Modules/Demo/Model.php
+++ Modules/Demo/Model.php
@@ -6,32 +6,38 @@ $demo = $autoSystem->create(
 		'bd'				=> 'demo',
 		'auto_increment'	=> 'id_demo',
 		'Fields'	=> 
-		array(
-		
-			'campo'		=>	array(
-				'Label'		=>	'Label Campo',
-				'Type'		=>	'varchar',
-				'Lenght'	=>	100
-			),
+			array(
+			
+				'campo'		=>	array(
+					'Label'		=>	'Label Campo',
+					'Type'		=>	'varchar',
+					'Lenght'	=>	100
+				),
 
-			'campo2'	=>	array(
-				'Label'		=>	'Label Campo 2',
-				'Type'		=>	'varchar',
-				'Lenght'	=>	100
-			),
+				'campo2'	=>	array(
+					'Label'		=>	'Label Campo 2',
+					'Type'		=>	'varchar',
+					'Lenght'	=>	100
+				),
 
-			'campo3'	=>	array(
-				'Label'		=>	'Label Campo 3',
-				'Type'		=>	'varchar',
-				'Lenght'	=>	50
-			),
+				'campo3'	=>	array(
+					'Label'		=>	'Label Campo 3',
+					'Type'		=>	'varchar',
+					'Lenght'	=>	50
+				),
 
-			'campo4'	=>	array(
-				'Label'		=>	'Label Campo 4',
-				'Type'		=>	'varchar',
-				'Lenght'	=>	10
+				'campo4'	=>	array(
+					'Label'		=>	'Label Campo 4',
+					'Type'		=>	'varchar',
+					'Lenght'	=>	10
+				),
+
+				'campo5'	=>	array(
+					'Label'		=>	'Label Campo 5',
+					'Type'		=>	'varchar',
+					'Lenght'	=>	255
+				),
 			),
-		),
 		'Grid'	=> array(
 			'Width'	=> '100%'
 		)
